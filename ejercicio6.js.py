c12064bff6ef2cac67a5b2f0bf2bb80cd0a9c0f3
Elvisuriana <eurianau@estudiante.ibero.edu.co>
2025-05-20 13:21:24 -0500

    Update ejercicio6.js.py

--- ejercicio6.js.py
+++ ejercicio6.js.py
@@ -1,9 +1,22 @@
-# Pedir un número entre 0 y while True:
-    try:
-        numero = int(input("Ingresa un número entre 0 y 10: "))
-        if 0 <= numero <= 10:
-            break
-        else:
-            print("❌ El número debe estar entre 0 y 10.")
-    except ValueError:
-        print("❌ Entrada inválida. Ingresa un número entero.")
+// funcion para mostrar la tabla de multiplicar
+function mostrarTablaMultiplicar(numero) {
+    console.log('tabla de multiplicar del ${numero}:');
+    for (let i = 0; i <= 10; i++) {
+        console.log ('${numero} x ${i} = ${numero * i}');
+
+
+
+
+    }
+}
+
+// pedir al usuario un numero entre 0 y 10
+const numero = parseInt(prompt("introduce un numero entre 0 y 10:"));
+// validar que el numero este en el rango correcto
+if (numero >= 0 && numero <=10) {
+    mostrarTablaMultiplicar(numero);
+} else {
+    console.log("el numero debe estar entre 0 y 10.");
+    
+}
+
